d99a31934496e9e085fde98c6fbb1a7fdfa2dd3a
sergeysychevskiy <sergey.sychevskiy@stryber.com>
2022-07-01 18:01:02 +0300

    Updated routes types

--- src/navigation/Routes.ts
+++ src/navigation/Routes.ts
@@ -2,3 +2,14 @@ export enum Routes {
   Home = 'Home',
   LogIn = 'LogIn',
 }
+
+export type RootStackParamList = {
+  [Routes.Home]: undefined;
+  [Routes.LogIn]: undefined;
+};
+
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
